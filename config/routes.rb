77bb90c55a58b9ef74bd36c07ae950bcc31424a1
kagron <kagron12@gmail.com>
2017-10-27 08:33:02 -0500

    Changed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'irb/home'
+  root 'irb#home'
 
-  get 'irb/about'
+  get 'about', to: 'irb#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
