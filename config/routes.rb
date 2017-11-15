b390bcb372e898bcae568d42977fc7cb8e9400c1
Kyle Grondin <kagron12@gmail.com>
2017-11-15 09:32:20 -0600

    Started styling the form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :documents
+  resources :applications, as: 'documents', controller: 'documents'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :admins
   get 'irb/FormApps'
