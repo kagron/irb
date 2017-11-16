1cc7723036d8422351b66413208323e9bcf78425
Kyle Grondin <kagron12@gmail.com>
2017-11-15 21:58:45 -0600

    Added a lot of styling and jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,25 @@
 //= require jquery3
 $(document).on('turbolinks:load', function(){
   $('.alert').delay(2000).fadeOut(2000);
+  $('#document_risks_label').hide();
+  $('#document_risks').hide();
+  $('#document_child_assent_file').hide();
+  $('#child_assent_label').hide();
+
+  $('#yesRisks').on('change', function() {
+    $('#document_risks_label').show(500);
+    $('#document_risks').show(500);
+  });
+  $('#noRisks').on('change', function() {
+    $('#document_risks_label').hide(500);
+    $('#document_risks').hide(500);
+  });
+  $('#yesMinors').on('change', function() {
+    $('#document_child_assent_file').show(500);
+    $('#child_assent_label').show(500);
+  });
+  $('#noMinors').on('change', function() {
+    $('#document_child_assent_file').hide(500);
+    $('#child_assent_label').hide(500);
+  });
 });
