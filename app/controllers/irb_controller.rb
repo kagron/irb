75a16aaba61b319549775773aa782c62ff2ca673
Kyle Grondin <kagron12@gmail.com>
2017-12-06 19:28:03 -0600

    Color Coding based on state

--- app/controllers/irb_controller.rb
+++ app/controllers/irb_controller.rb
@@ -1,11 +1,5 @@
 class IrbController < ApplicationController
-  def FormApps
-    if params[:search]
-      @data = Document.search(params[:search]).order("created_at DESC")#.where(:is_archived => '0') #1isArchived
-    else
-      @data = Document.all.order("created_at DESC")#.where(:is_archived => '0')
-    end
-  end
+
 
   def ArchivedApps
     if params[:search]
@@ -15,11 +9,6 @@ class IrbController < ApplicationController
     end
   end
 
-  def InProgressApps
-  end
-
-  def StateApps
-  end
 
   def self.search(search)
 	   where("id LIKE ? OR fName LIKE ? OR lName LIKE ? OR phone LIKE ? OR email LIKE ? OR department LIKE ? OR typeOfApplication LIKE ? OR project_title LIKE ? OR sponsor_name LIKE ? OR start_date LIKE ? OR end_date LIKE ? OR state LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,9 @@ Rails.application.routes.draw do
   resources :applications, as: 'documents', controller: 'documents'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :admins
-  get 'irb/FormApps'
 
   get 'irb/ArchivedApps'
 
-  get 'irb/InProgressApps'
-
-  get 'irb/StateApps'
-
-  get 'form_apps/ArchivedApps'
-
-  get 'form_apps/InProgressApps'
-
-  get 'form_apps/StateApps'
-
-  get 'irb/BoardView'
 
   devise_for :users
   root 'irb#home'
