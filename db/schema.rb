bf5ccbd21ae9a79265cd3bfe0929402c33eb8ce4
bpichen01 <bpichen01@aurora.edu>
2018-01-19 10:20:21 -0600

    Changed tabs for user/supervisor

--- db/schema.rb
+++ db/schema.rb
@@ -77,13 +77,6 @@ ActiveRecord::Schema.define(version: 20180115224242) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "superadmin_role"
@@ -91,9 +84,7 @@ ActiveRecord::Schema.define(version: 20180115224242) do
     t.boolean "user_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
-  add_foreign_key "articles", "users"
   add_foreign_key "documents", "users"
 end
