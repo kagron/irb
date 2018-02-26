1d05a43a9325cd48113f8e3bf000ca2db16043b2
Kyle Grondin <kagron12@gmail.com>
2018-02-26 10:13:40 -0600

    Can do single assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,13 +2,13 @@ class AssignmentsController < ApplicationController
 
   def create
     @users = User.where(supervisor_role: '1')
-    @assignment = Assignment.new(assignment_params)
-    @assignment.document_id = params[:document_id]
-    @assignment.user_id = params[:user_id]
+    @assignment = Assignment.new()
+    @assignment.document_id = params[:document_id][0]
+    @assignment.user_id = params[:user_id][0]
 
 
     respond_to do |format|
-      if @assignment.save
+      if @assignment.save!
         format.html { redirect_to applications_new_apps_path, notice: 'Document assigned succesfully' }
         format.json { render :show, status: :created, location: @assignment }
       else
@@ -20,6 +20,6 @@ class AssignmentsController < ApplicationController
 
   private
     def assignment_params
-      params.require(:assignment).permit(:documents_id,:users_id)
+      params.require(:assignment).permit(:document_id,:user_id)
     end
 end
