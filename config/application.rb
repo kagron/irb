db6e693125c0e650d61c91e08aa6a4ce9c9e7d2a
Kyle Grondin <kagron12@gmail.com>
2018-04-23 22:24:42 -0500

    Force SSL

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Irb
     config.load_defaults 5.1
     config.time_zone = 'Central Time (US & Canada)'
     config.active_record.default_timezone = :local
+    config.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
